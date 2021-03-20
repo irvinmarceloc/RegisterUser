6d2464a49dabe1005d1aa68c2bdbd4e7c3ce7f92
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-20 04:04:55 -0300

    frontend4

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
-export const Users = () => (
-    <h1>Users</h1>   
-)
+export const Users = () => {
+    
+    const handleSubmit = (e) => {
+        console.log(e)
+    }
+
+    return (
+        <div className="row">
+            <div className="col-md-4">
+                <form onSubmit="{handleSubmit}">
+                    
+                </form>
+            </div>
+            <div className="col md-8">
+
+            </div>
+        </div>
+    )   
+}
