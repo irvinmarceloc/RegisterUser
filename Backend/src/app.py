5045825811ced0c59b25f7fab9b5b1583d18dd07
irvinmarceloc <irvinmarceloc@gmail.com>
2021-03-20 00:50:52 -0300

    crud users 1

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_pymongo import PyMongo
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo, ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,12 +10,24 @@ db = mongo.db.users
 
 @app.route('/users', methods=['POST'])
 def createUser():
-    print(request.json)
-    return 'received'
+    id = db.insert({
+        'name':  request.json['name'],
+        'email':  request.json['email'],
+        'password':  request.json['password']
+    })
+    return jsonify(str(ObjectId(id)))
 
 @app.route('/users', methods=['GET'])
 def getUsers():
-    return 'received'
+    users = []
+    for doc in db.find():
+        users.append({
+            '_id': str(ObjectId(doc['_id'])),
+            'name'   : doc['name'],
+            'email': doc['email'],
+            'password':doc['password']
+        })
+    return jsonify(users)
 
 @app.route('/users/<id>', methods=['GET'])
 def deleteUser():
